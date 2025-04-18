8bce5bd7c9cddc3ef29168565e4f2eed7badc272
emerytown <ztown@outlook.com>
2025-04-18 10:44:47 -0700

    readme updated

--- PA9_Tai_Town/PA9_Tai_Town/mole.hpp
+++ PA9_Tai_Town/PA9_Tai_Town/mole.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
+
 class Mole : public sf::Sprite {
 public:
 	Mole(const sf::Texture texture, const sf::Vector2f& pos) : sf::Sprite(texture){
@@ -8,8 +9,8 @@ public:
 		isVisible = false;
 	}
 private:
-	bool isVisible;
-	int hiddenDuration;
-	int visibleDuration;
-	bool hitState;
+	bool isVisible; // if isVisible is false then the click doesn't count towards the score 
+	int hiddenDuration; // amount of time mole is hidden
+	int visibleDuration; // amount of time mole is visible and able to be hit 
+	bool hitState; // if mole was hit or not 
 };
