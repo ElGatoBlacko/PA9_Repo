915ce8f745f05b743f3086ba08ed04e44cf26fef
ElGatoBlacko <thebeaubs@icloud.com>
2025-04-19 21:18:36 -0700

    timer

--- PA9_Tai_Town/PA9_Tai_Town/main.cpp
+++ PA9_Tai_Town/PA9_Tai_Town/main.cpp
@@ -8,8 +8,17 @@ int main(void) {
 
     sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML works!");
     sf::Texture moleTexture;
+    sf::Font font;
   //  std::cout << "Working directory: " << std::filesystem::current_path() << std::endl;
     moleTexture.loadFromFile("mole_image.png");
+    font.openFromFile("C:\\Users\\ariet\\source\\repos\\PA9_Repo_Spot\\PA9_Tai_Town\\PA9_Tai_Town\\WinkyRough-VariableFont_wght.ttf");
+
+    sf::Clock gameClock;
+    std::string time = "";
+
+    sf::Text runtime(font, time, 30);
+    runtime.setFillColor(sf::Color::White);
+    runtime.setPosition(sf::Vector2f(50, 0));
 
     std::vector<Mole> moles;
 
@@ -18,9 +27,10 @@ int main(void) {
     }
 
     int spaceOut = -1;
-    sf::Clock gameClock;
+    
     while (window.isOpen())
     {
+        
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>()) {
@@ -41,10 +51,14 @@ int main(void) {
 
         
         window.clear();
+
+        time = std::to_string(gameClock.getElapsedTime().asSeconds());
+        runtime.setString(time);
         for (int i = 0; i < spaceOut && i <10; i++) {
             window.draw(moles[i]);
             moles[i].update();
         }
+        window.draw(runtime);
         window.display();
         
         spaceOut= gameClock.getElapsedTime().asSeconds()-1;
