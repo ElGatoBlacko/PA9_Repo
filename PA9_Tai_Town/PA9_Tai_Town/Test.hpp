47ac96169643cddd827458eb8568f81d2277ab5d
ElGatoBlacko <thebeaubs@icloud.com>
2025-04-22 12:13:20 -0700

    read me

--- PA9_Tai_Town/PA9_Tai_Town/Test.hpp
+++ PA9_Tai_Town/PA9_Tai_Town/Test.hpp
@@ -1,12 +1,3 @@
-// test cases for basic automation and conditional checks 
-// does not need to be complex test cases or complex aspects of the game 
-
-// video 
-// show the game 
-// then show quick run through of the code 
-// explain choices we made 
-// possible additions if we had more time 
-
 #pragma once
 #include "Mole.hpp"
 #include<iostream>
